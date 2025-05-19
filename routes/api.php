448ad5a9451f018e67ad1746d5bd7f27f495bba1
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2025-05-19 23:35:37 +0300

    Add auth user request for products

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'createUser']);
 Route::post('login', [AuthController::class, 'loginUser']);
 
-// 1. Public read‐only
 Route::get('products',              [ProductController::class, 'index']);
 Route::get('products/{product}',    [ProductController::class, 'show']);
 Route::get('categories',            [ProductController::class, 'index']);
 Route::get('categories/{category}', [ProductController::class, 'show']);
 
-
-// 2. Protected create/update/delete
 Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('categories', CategoryController::class)
-         ->except(['index','show']);
-    Route::apiResource('products',   ProductController::class)
-         ->except(['index','show']);
+    Route::post('products', [ProductController::class, 'store']);
 });
