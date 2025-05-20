40110add7c314d3676a3ed91b6031a72096cda93
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2025-05-20 17:31:09 +0300

    Update facory - test fixes

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
@@ -16,9 +17,16 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+   static $uniqueFaker;
+        if (! $uniqueFaker) {
+            $uniqueFaker = $this->faker->unique();
+        }
+
+        $base = $uniqueFaker->word();
+
         return [
-                'name'        => $this->faker->unique()->word,
-                'description' => $this->faker->sentence,
+            'name'        => $base . '_' . Str::random(5),
+            'description' => $this->faker->sentence(),
         ];
     }
 }
